488487f1493051b7bce68663e6931bbd16e4654c
Satish Parmar <Satish Parmar1>
2023-08-02 06:10:28 +0530

    day10

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
   render() {
-    let{type ,desc,link,img}= this.props;
+    let{tittle ,desc,img,buttonurl}= this.props;
     return (
-        <><div className="card" style={{width: "18rem"}}>
-                <img src="https://www.hindustantimes.com/ht-img/img/2023/04/21/1600x900/CRICKET-IND-IPL-T20-PUNJAB-BANGALORE-36_1682063217086_1682063468089.jpg" className="card-img-top" alt="..."/>
-                <div className="card-body">
-                <h5 className="card-title">{type}</h5>
-                <p className="card-text">{desc}</p>
-                <a href="/news" className="btn btn-primary">Go and watch</a>
-        </div>
+        <>
+              <div className="card" style={{width: "18rem"}}>
+              <img src={img} className="card-img-top" alt="..."/>
+                <div className="card-body" key={buttonurl}>
+                <h5 className="card-title">{tittle}...</h5>
+                <p className="card-text">{desc}...</p>
+                <a href={buttonurl} className="btn btn-sm btn-primary">Go and Read</a>
+                </div>    
       </div>
       </>
     )
