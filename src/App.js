86b9916f832f93657c758f33f73ec0ef1fb71b72
Satish Parmar <Satish Parmar1>
2023-08-13 09:12:25 +0530

    day20

--- src/App.js
+++ src/App.js
@@ -28,28 +28,28 @@ export default class App extends Component {
       />
      
       <Routes>
-      <Route exact path=""  key="general "  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="general"/>  }/>    
+      <Route exact path="/30DayofCode-news-app"  key="general "  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="general"/>  }/>    
      </Routes>
       <Routes>
-      <Route exact path="/general" key="general" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="general"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/general" key="general" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="general"/>  }/>    
      </Routes>
      <Routes>
-      <Route exact path="/entertainment" key="entertainment" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="entertainment"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/entertainment" key="entertainment" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="entertainment"/>  }/>    
      </Routes>
      <Routes>
-      <Route exact path="/health" key="health" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="health"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/health" key="health" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="health"/>  }/>    
      </Routes>
      <Routes>
-      <Route exact path="/science" key="science"  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="science"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/science" key="science"  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="science"/>  }/>    
      </Routes>
      <Routes>
-      <Route exact path="/sports" key="sports"  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="sports"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/sports" key="sports"  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="sports"/>  }/>    
      </Routes>
      <Routes>
-      <Route exact path="/technology" key="technology"  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="technology"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/technology" key="technology"  element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="technology"/>  }/>    
      </Routes>
      <Routes>
-      <Route exact path="/business" key="business" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="business"/>  }/>    
+      <Route exact path="/30DayofCode-news-app/business" key="business" element={  <News setProgress = {this.setProgress} pagesize={30}  country="in" newstype="business"/>  }/>    
      </Routes>
      
       </Router>     
